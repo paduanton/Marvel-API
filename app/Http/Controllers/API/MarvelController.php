87f0383e3a229dc6ac66b8361c72181371d59610
paduanton <antonio.junior.h@gmail.com>
2019-09-09 01:26:53 -0300

    adicionando endpoint para buscar histórias e finalizando estrutura básica do método getHistorias()

--- app/Http/Controllers/API/MarvelController.php
+++ app/Http/Controllers/API/MarvelController.php
@@ -15,14 +15,16 @@ class MarvelController extends Controller
 //        $cliente = new \GuzzleHttp\Client();
     }
 
-    public function gera_hash($timestamp) {
+    public function gera_hash($timestamp)
+    {
         $chave_publica = config('app.marvel_publickey');
         $chave_privada = config('app.marvel_privatekey');
 
-        return md5($timestamp.$chave_privada.$chave_publica);
+        return md5($timestamp . $chave_privada . $chave_publica);
     }
 
-    public function getPersonagemId($nome) {
+    public function getPersonagemId($nome)
+    {
         $cliente = new \GuzzleHttp\Client();
         $timestamp = uniqid(); // Carbon::now();
         $hash = $this->gera_hash($timestamp);
@@ -30,7 +32,7 @@ class MarvelController extends Controller
         $chave_publica = config('app.marvel_publickey');
         $url_base = config('app.marvel_url');
 
-        $res = $cliente->request('GET', $url_base.'/v1/public/characters', [
+        $response_body = $cliente->request('GET', $url_base . '/v1/public/characters', [
             'query' => [
                 'apikey' => $chave_publica,
                 'hash' => $hash,
@@ -40,7 +42,7 @@ class MarvelController extends Controller
             ]
         ]);
 
-        $response = $res->getBody();
+        $response = $response_body->getBody();
 
         $personagem = json_decode($response, true);
         $id = $personagem['data']['results'][0]['id'];
@@ -50,72 +52,52 @@ class MarvelController extends Controller
         ], 200);
     }
 
-    public function getHistorias($id_personagem) {
-
-    }
-/*
-    public function getHistoriasInfo($id_personagem) {
-
-    }*/
-
-    public function getQuadrinhos($id_historia) {
-
-    }
-
-
-
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function getHistorias($id_personagem)
     {
-        //
+        $cliente = new \GuzzleHttp\Client();
+        $timestamp = uniqid(); // Carbon::now();
+        $hash = $this->gera_hash($timestamp);
+
+        $chave_publica = config('app.marvel_publickey');
+        $url_base = config('app.marvel_url');
+
+        $response_body = $cliente->request('GET', $url_base . '/v1/public/characters/' . $id_personagem . '/stories', [
+            'query' => [
+                'apikey' => $chave_publica,
+                'hash' => $hash,
+                'ts' => $timestamp,
+                'limit' => 5,
+                'orderBy' => 'id'
+            ]
+        ]);
+
+        $response = $response_body->getBody();
+
+        $historias = json_decode($response, true);
+        $info_historias = $historias['data']['results'];
+
+        foreach ($info_historias as $key => $value) {
+            $response_historias[$key] = array
+            (
+                'id' => $value["id"],
+                'titulo' => $value["title"],
+                'tipo' => $value["type"],
+                'data_modificacao' => $value["modified"],
+                'num_criadores' => $value["creators"]["available"],
+                'num_series' => $value["series"]["available"],
+                'num_quadrinhos' => $value["comics"]["available"],
+                'num_herois' => $value["characters"]["available"],
+                'num_eventos' => $value["events"]["available"]
+            );
+        }
+        return response()->json([
+            'historias' => $response_historias
+        ], 200);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function getQuadrinhos($id_historia)
     {
-        //
+
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
